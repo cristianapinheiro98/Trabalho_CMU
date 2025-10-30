f87e71352f21865630c4b32bd8199c7e864d6894
Maria Eduarda Sameiro <8230227@estg.ipp.pt>
2025-10-30 09:41:21 +0000

    changes

--- app/src/main/java/pt/ipp/estg/trabalho_cmu/data/local/entities/Animal.kt
+++ app/src/main/java/pt/ipp/estg/trabalho_cmu/data/local/entities/Animal.kt
@@ -1,7 +1,5 @@
 package pt.ipp.estg.trabalho_cmu.data.local.entities
 
 data class Animal(
-    val nome: String,
-    val idade: Int
 
 )
--- app/src/main/java/pt/ipp/estg/trabalho_cmu/data/remote/dtos/VetResponse.kt
+++ app/src/main/java/pt/ipp/estg/trabalho_cmu/data/remote/dtos/VetResponse.kt
@@ -1,6 +1,4 @@
 package pt.ipp.estg.trabalho_cmu.data.remote.dtos
 
 data class VetResponse(
-    val name: String,
-    val location: String
 )
